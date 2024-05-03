859dfa29133cebe62c2e16958ef0573eb49d5a44
Fahmi Wahyu Wiradika <fahmi.wiradika@edmi-meters.com>
2024-05-03 16:25:15 +0700

    first commnit

---
+++ public/js/ekobayu.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) {
+
+  // navbar fixed and Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Initiate the wowjs animation library
+  new WOW().init();
+
+  // Initiate superfish on nav menu
+  $('.nav-menu').superfish({
+    animation: {
+      opacity: 'show'
+    },
+    speed: 800
+  });
+
+});
